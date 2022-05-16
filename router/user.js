f81a140caf058dfa2e24d1c9ba57344cb82ad02b
CHEON <yongcheon543218@outlook.com>
2022-05-16 23:05:53 +0800

    第四次提交了weTravel项目文件

--- router/user.js
+++ router/user.js
@@ -16,19 +16,32 @@ router.post("/reg",(req,res)=>{
 	})
 })
 
-//用户登录 查询数据  //接口测试通过
-router.get("/log",(req,res)=>{
+//查询用户名是否存在 //接口测试通过
+router.get("/select",(req,res)=>{
+	const uname=req.query.uname
+	const n="select uid from w_user where uname=?"
+	pool.query(n,uname,(err,result)=>{
+		if(err){
+			throw err
+			return
+		}
+	result.length>0 ? res.send({code:200,msg:"用户名已经存在"}) : res.send({code:201,msg:"用户名不存在"})		
+	})
+})
+
+//用户登录 查询数据  //接口测试不
+router.post("/log",(req,res)=>{
 	const uname=req.body.uname
-	const upwd=req.body.uname
+	const upwd=req.body.upwd
 	//const upwd=req.body.upwd
-	//console.log(uname)
+	//console.log(upwd)
 	const n=`select upwd from w_user where uname=?` 
 	pool.query(n,uname,(err,result)=>{
 		if(err){
 			throw err
 			return
-		}			
-		//console.log(result.upwd)
-		result.upwd==upwd ? res.send({code:200,msg:`登录成功!欢迎${uname}`}) : res.send({code:201,msg:"登录失败!"})
+		}	
+
+		result==upwd ? res.send({code:200,msg:`登录成功!欢迎${uname}`}) : res.send({code:201,msg:"登录失败!"})
 	})
 })
